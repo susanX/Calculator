711f6deb1573dbbd97823ea01d8e076e1f75c6ee
susanX <susan.gooding@yahoo.co.uk>
2018-10-09 12:55:27 +0100

    button parent

--- calc.js
+++ calc.js
@@ -1,2 +1,13 @@
+function modifyText(e) {
+  //current target is the box
+  if(e.target !== e.currentTarget){
+  console.log(e.target.value);
+  }
+//make box no click
+e.stopPropagation();
+}
+// add event listener to table
+var numb = document.getElementById("box");
+numb.addEventListener("click", modifyText, false);
 
-//start
+// TODO https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Legacy_Internet_Explorer_and_attachEvent
