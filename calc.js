5eb83642a9c27853fe94eeff93d094d26d480761
susanX <susan.gooding@yahoo.co.uk>
2018-10-09 16:41:23 +0100

    index

--- calc.js
+++ calc.js
@@ -1,13 +1,18 @@
-function modifyText(e) {
+(function () {
+    'use strict';
+   // this function is strict...
+}());
+
+function modifyText (e) {
   //current target is the box
-  if(e.target !== e.currentTarget){
-  console.log(e.target.value);
-  }
+     if ( 'use strict'; e.target !== e.currentTarget ){
+     console.log(e.target.value);
+     }
 //make box no click
-e.stopPropagation();
+e.stopPropagation ();
 }
 // add event listener to table
-var numb = document.getElementById("box");
-numb.addEventListener("click", modifyText, false);
+var numb = document.getElementById ("box");
+numb.addEventListener ( "click", modifyText, false );
 
 // TODO https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Legacy_Internet_Explorer_and_attachEvent
